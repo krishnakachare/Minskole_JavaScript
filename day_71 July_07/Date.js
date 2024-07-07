6f69514060c00295c6e59ac959ba89884f3ba87d
shrikrishnaMinksole <shrikrishna.minksole@gmail.com>
2024-07-07 09:51:01 +0530

    Date

---
+++ day_71 July_07/Date.js
@@ -0,0 +1,7 @@
+// Date and Time in Js :
+
+let dateObj = new Date();
+console.log(dateObj) // Sun Jul 07 2024 09:37:10 GMT+0530 (India Standard Time)
+console.log(typeof(dateObj)) // object --> Abstraction
+// console.log([])
+// console.log(typeof([])) // object
