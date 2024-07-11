5be6cbe3a4042d3b040b0579ce4d6cf28dff0b63
shrikrishnaMinksole <shrikrishna.minksole@gmail.com>
2024-07-11 10:18:23 +0530

    JSON Methods

---
+++ day_75 July_11/json.js
@@ -0,0 +1,25 @@
+/* 
+# Datatypes in JSON: string, number, boolean, null, array, object
+
+# JSON methods:
+    1. JSON.stringify()
+    2. JSON.parse() 
+*/
+
+let obj = {
+    name: "JS",
+    age: 10,
+    isTrue: true
+}
+console.log(obj) // {name: 'JS', age: 10, isTrue: true}
+console.log(typeof (obj)) // object
+let strObj = JSON.stringify(obj)
+console.log(strObj) // '{"name":"JS","age":10,"isTrue":true}'
+console.log(typeof (strObj)) // string
+
+
+let jsObj = JSON.parse(strObj)
+console.log(jsObj) // { name: 'JS', age: 10, isTrue: true } 
+console.log(typeof(jsObj))  // object
+
+// js obj 
