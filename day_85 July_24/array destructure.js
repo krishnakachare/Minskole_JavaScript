3d0e243dcf62fea55821c48f0863a319ffceb4f4
shrikrishnaMinksole <shrikrishna.minksole@gmail.com>
2024-07-24 10:32:18 +0530

    array destructure

---
+++ day_85 July_24/array destructure.js
@@ -0,0 +1,71 @@
+/* 
+# Destructuring:
+    ES6 feature
+    Can destructure object --> array[], string'', object{}
+    De-structure or braking object in seperate address-value pair
+
+    For array: address(index no)-value pair
+
+    array destructure syntax :
+        let [first,second,third]=arrayname/array 
+                mutipal address = multipal values
+*/
+
+// let a = 10
+
+let str = "JavaScript"
+
+let arr = [1, 2, 3]
+//         0  1  3
+arr[1] // 2 --> Retrive values
+
+let obj = {
+    name: "JS",
+    age: 10
+}
+obj.age // 10 --> Retrive values
+
+// Array : Position is mandetory, not variable name
+let [a, b, c] = [1, 2, 3]
+console.log(a) // 1
+console.log(b) // 2
+console.log(c) // 3
+
+// can skip the values
+let [p,, q] = [1, 2, 3]
+console.log(p) // 1
+console.log(q) // 3
+
+// can assign the default values
+let [m,, n, t = 20] = [1, 2, 3]
+console.log(t) // undefined // 20 --> default value
+
+// swaip or exchange the variable values 
+// var aa = 10;
+// var bb = 20;
+// console.log("aa", aa) // 10
+// console.log("bb", bb) // 20
+// var [bb, aa] = [aa, bb]
+// console.log("aa", aa) // 20
+// console.log("bb", bb) // 10
+
+let aa = 10;
+let bb = 20;
+
+console.log("aa", aa); 
+console.log("bb", bb); 
+
+[aa, bb] = [bb, aa];
+
+console.log("aa", aa); 
+console.log("bb", bb); 
+
+//-------------------------------------------------
+// object :
+// syntax : {}
+
+const { age } = obj
+console.log(age) // 10
+
+// real time example:
+// const {test, expect} = require("@playwrightTest")
