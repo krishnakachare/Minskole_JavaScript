b0a471d717bd474b7c1b193cd64521ba46f8370b
krishnakachare <shrikrishna.minksole@gmail.com>
2025-12-19 21:21:30 +0530

    Day_2

---
+++ Day_2/first.js
@@ -0,0 +1,16 @@
+// HTML, CSS , JS --> Browser technologies
+
+console.log("Hello World"); // Hello World
+console.log(10 + 10); // 20
+
+/* 
+Run js file:   
+- Right click on the file & select option 'open in integrated terminal'
+- Can see terminal at the bottom with path of the file (parent folder path)
+- Type command > node fileName.js
+- save code --> ctrl+s
+*/
+
+console.log("I am External JS file");
+// Can see output in browser console as well as terminal
+
