068284f2f8b6bca9d222b4a9204a2b3504b3c747
krishnakachare <shrikrishna.minksole@gmail.com>
2025-12-22 21:20:36 +0530

    Day_3

---
+++ Day_3 Dec_22/codeFirst.js
@@ -0,0 +1,38 @@
+// console.log("Hello World"); // Hello World
+
+// data types:
+// "Hello World"  -- > string
+// 10 -- > number
+// true / false -- > boolean
+
+// variables --> container or Memory location/address
+
+// read code:
+a = 10;
+// a --> variable
+// = --> operator
+// 10 --> value
+
+b = "Hello";
+// console.log("Hello"); // --> "Hello"
+// console.log(b);  // --> Hello
+
+// variable declaration keywords:
+// var, let, const
+
+// c = "Hello";
+// In javascript best practice is to declare variable with var, let or const keyword
+
+// let c = "Hello"; // c variable declared with let keyword
+// console.log(c);  // --> Hello
+
+// const c = "Hello"; // c variable declared with const keyword
+// console.log(c);  // --> Hello
+
+var c = "Hello"; // c variable declared with var keyword
+console.log(c);  // --> Hello
+
+
+// Think: Why we have 3 keywords to declare variable?
+// Answer: Each keyword has its own importance and usecase in real world application development.       
+// Next file: varLetConst.js
