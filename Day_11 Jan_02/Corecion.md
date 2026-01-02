77e3db76594cf2fedfebceca59599e99505dfa0e
krishnakachare <shrikrishna.minksole@gmail.com>
2026-01-02 22:24:07 +0530

    Day_11

---
+++ Day_11 Jan_02/Corecion.md
@@ -0,0 +1,8 @@
+/* 
+# Corecion : forcefully datatype conversion for perform operation
+  1. comparision operator: converting into number datatype
+  2. Arithmetic operations:
+       1. string: + (string concatination): simply addition of string
+       2. string: (-*/) Datatype convert into number & with string perform mathematical operations.
+       3. For unary operands + operator will convert datatype into number.
+*/
---
+++ Day_11 Jan_02/corecion.js
@@ -0,0 +1,59 @@
+/* 
+    +  : converting datatype in number
+
+    What is Corecion ?
+    Forcefully datatype conversion for perform operation.
+
+    Why is Corecion ?
+    Able to perform mathematical operations
+
+    Why converting in only number ?
+    To perform mathematical operations we required values from number datatype
+
+*/
+
+// string addition called as string concatination:
+// console.log(1 + 1) // 2
+
+// console.log("Hi" + "Js") // HiJs
+
+// console.log(10 + "Js") // 10Js
+
+// console.log(10 + "1"); // 101
+
+// Corecion with + operator:
+// console.log(10 + +"1"); // 11
+
+// Corecion 
+// console.log(10 - "1"); // 9
+console.log(10 - "Hi"); // NaN
+// console.log(10 * "1"); // 10
+// / % **
+
+// console.log(10 - "Js"); // NaN
+
+// console.log(10 > "Js"); // false
+// Always tring to convert in number datatype
+// "Js" --> NaN
+//Always comaprision with NaN is false
+
+// console.log('10' < "Js"); 
+
+/* 
+    '10' is a string
+
+    "Js" is a string
+
+    JavaScript compares them character by character using Unicode values
+
+    Comparison starts with the first character:
+
+    '1' → Unicode 49
+
+    'J' → Unicode 74
+
+    49 < 74  → true
+*/
+
+
+
