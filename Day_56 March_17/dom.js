badc2faf46edc0f11a5e59d35fa01eeb9bfd3bd7
krishnakachare <shrikrishna.minksole@gmail.com>
2026-03-17 21:13:51 +0530

    Day_56

---
+++ Day_56 March_17/dom.js
@@ -0,0 +1,21 @@
+// DOM
+
+// UI --> HTML, CSS, JS
+
+// HTML 
+
+
+<input class="gNO89b" value="Google Search" ></input>
+
+// CAN perform CURD in HTML with the help of JS
+// JS --> 
+
+
+let obj = {
+    //
+    //
+    //
+    a : 10
+}
+
+// obj.a --> Address of a
