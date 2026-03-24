80c3ff441877ac867073fe18642d7c9554762abc
krishnakachare <shrikrishna.minksole@gmail.com>
2026-03-24 21:14:04 +0530

    Day_60

---
+++ Day_60 March_24/createElement.js
@@ -0,0 +1,16 @@
+console.log(document)
+
+let el = document.createElement("div");
+console.log(el);
+
+el.textContent = "I am div"
+console.log(el);
+
+document.body.appendChild(el)
+
+// document.write("Hi I am writting....");
+let div = document.getElementsByClassName("Three");
+
+document.removeChild("h1");
+
+console.log(document)
